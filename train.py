f9dd488b71ed36d441ed1ad799daa71c50779a4e
NayonGit <yoann.roch06@gmail.com>
2026-04-06 00:44:12 +0200

    final train file

--- train.py
+++ train.py
@@ -1,23 +1,30 @@
 import os
 import argparse
+from pyexpat import model
 import h5py
 import lightning as L
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, RichProgressBar, LearningRateMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
-from lightning.pytorch.callbacks import StochasticWeightAveraging
 from torch.utils.data import DataLoader
 import torch
 
-from data import HistopathDataset, get_transforms, get_transforms_curia, get_transforms_dinov2
-from models import ContrastiveHDFFModule, CuriaModule , HDFFModule, Dinov2Module, HistopathLightningModule
+from data import HistopathDataset, get_transforms_fms
+from models import UNI2Module, PhikonV2Module, Dinov2Module, GigaPathModule, Virchow2Module
 
+MODEL_REGISTRY = {
+    "uni2": UNI2Module,
+    "phikonv2": PhikonV2Module,
+    "dinov2": Dinov2Module,
+    "gigapath": GigaPathModule,
+    "virchow2": Virchow2Module
+}
 
 def train(args):
 
     torch.set_float32_matmul_precision('high')
     
-    train_ds = HistopathDataset(args.train_path, transforms=get_transforms_dinov2(mode='train'))
-    val_ds = HistopathDataset(args.val_path, transforms=get_transforms_dinov2(mode='val'))
+    train_ds = HistopathDataset(args.train_path, transforms=get_transforms_fms(mode='train'))
+    val_ds = HistopathDataset(args.val_path, transforms=get_transforms_fms(mode='val'))
 
     train_loader = DataLoader(
         train_ds, 
@@ -33,28 +40,19 @@ def train(args):
         num_workers=args.num_workers
     )
 
-    # # Model Initialization
-    # model = HistopathLightningModule(
-    #     lr=args.lr,
-    #     method="full"
-    # )
-
-    # model = ContrastiveHDFFModule(
-    #     lr=args.lr,
-    #     pretraining = args.pretraining
-    # )
-    # model = CuriaModule(
-    #     lr=args.lr,
-    # )
-    model = Dinov2Module(
-        lr=args.lr,
-    )
-
+    if args.model_name not in MODEL_REGISTRY:
+        raise ValueError(f"Model {args.model_name} unknown. Choose among: {list(MODEL_REGISTRY.keys())}")
+    
+    print(f"Model Initialization : {args.model_name}")
+    model_class = MODEL_REGISTRY[args.model_name]
+    model = model_class(lr=args.lr)
+    
     # Callbacks and Logger
-    checkpoint_dir = os.path.join("models", "dinov2_models_2")
+    checkpoint_dir = os.path.join("models", args.model_name)
+    logger_name = f"{args.model_name}_experiment"
     monitor_metric = "val/acc"
     mode = "max"
-    filename_format = "dinov2_2-model-{epoch:02d}"
+    filename_format = f"{args.model_name}-{{epoch:02d}}"
 
     checkpoint_callback = ModelCheckpoint(
         dirpath=checkpoint_dir,
@@ -72,7 +70,7 @@ def train(args):
         mode=mode
     )
 
-    logger = TensorBoardLogger("logs", name="dinov2_model")
+    logger = TensorBoardLogger("logs", name=logger_name)
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
 
     callbacks = [RichProgressBar(), lr_monitor, checkpoint_callback, early_stop_callback]
@@ -88,7 +86,7 @@ def train(args):
         deterministic=True
     )
     if args.resume_from and os.path.exists(args.resume_from):
-        print(f"♻️ Resuming training from checkpoint: {args.resume_from}")
+        print(f"Resuming training from checkpoint: {args.resume_from}")
         ckpt_path = args.resume_from
     else:
         if args.resume_from:
@@ -100,17 +98,17 @@ def train(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # Path (do not change the default paths, they are set to work with the provided data structure)
     parser.add_argument("--train_path", type=str, default="data/train.h5")
     parser.add_argument("--val_path", type=str, default="data/val.h5")
     
+    # Model Selection
+    parser.add_argument("--model_name", type=str, default="uni2", choices=list(MODEL_REGISTRY.keys()), 
+                        help="Model to train. Choose among: " + ", ".join(MODEL_REGISTRY.keys()))
     # Hyperparameters
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--epochs", type=int, default=50)
-    # parser.add_argument("--pretraining", action="store_true", help="Whether to use the pretraining phase with contrastive loss")
     
-    # Hardware
     parser.add_argument("--num_workers", type=int, default=4)
     parser.add_argument("--use_fp16", action="store_true", help="Use 16-bit precision")
     parser.add_argument("--resume_from", type=str, default=None, help="Path to a checkpoint (.ckpt) to resume from")
